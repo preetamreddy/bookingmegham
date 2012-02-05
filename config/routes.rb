8958be3cc599b4e8ed7654b61c7a601c1681f12d
Preetam Reddy <preetam.reddy@gmail.com>
2012-02-06 01:34:34 +0530

    Added login feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Bookingmegham::Application.routes.draw do
+	controller :sessions do
+		get 'login' => :new
+		post 'login' => :create
+		delete 'logout' => :destroy
+	end
+
   resources :users
 
   resources :advisors
