3d88e73f8b50f6ae2baced5a09de1d73ff024dce
Preetam Reddy <preetam.reddy@gmail.com>
2012-02-21 20:37:51 +0530

    Added additional recipients for mails - advisor, accounts and trip email ids

--- app/mailers/trip_notifier.rb
+++ app/mailers/trip_notifier.rb
@@ -1,31 +1,37 @@
 class TripNotifier < ActionMailer::Base
-  default from: "Banjara Camps <banjaracamps@ezbook.in>"
+	ACCOUNTS_EMAIL_ID = "accounts@banjaracamps.com"
+
+	default from: "Banjara Camps <banjaracamps@ezbook.in>"
 
   def details(trip)
 		@trip = trip
 
-    mail to: "#{@trip.guest.name} <#{@trip.guest.email_id}>", 
+		mail to: "#{@trip.guest.name} <#{@trip.guest.email_id}>", 
+    	cc: "#{@trip.email_ids}, #{@trip.advisor.name} <#{@trip.advisor.email_id}>",
 			subject: 'Your holiday with Banjara Camps - Trip details'
   end
 
   def invoice(trip)
 		@trip = trip
 
-    mail to: "#{@trip.guest.name} <#{@trip.guest.email_id}>", 
+		mail to: "#{@trip.guest.name} <#{@trip.guest.email_id}>", 
+			cc: "#{@trip.email_ids}, #{@trip.advisor.name} <#{@trip.advisor.email_id}>, #{ACCOUNTS_EMAIL_ID}",
 			subject: 'Your holiday with Banjara Camps - Invoice'
   end
 
   def receipt(trip)
 		@trip = trip
 
-    mail to: "#{@trip.guest.name} <#{@trip.guest.email_id}>", 
+		mail to: "#{@trip.guest.name} <#{@trip.guest.email_id}>", 
+			cc: "#{@trip.email_ids}, #{@trip.advisor.name} <#{@trip.advisor.email_id}>, #{ACCOUNTS_EMAIL_ID}",
 			subject: 'Your holiday with Banjara Camps - Receipt'
   end
 
   def voucher(trip)
 		@trip = trip
 
-    mail to: "#{@trip.guest.name} <#{@trip.guest.email_id}>", 
+		mail to: "#{@trip.guest.name} <#{@trip.guest.email_id}>", 
+			cc: "#{@trip.email_ids}, #{@trip.advisor.name} <#{@trip.advisor.email_id}>",
 			subject: 'Your holiday with Banjara Camps - Voucher'
   end
 end
