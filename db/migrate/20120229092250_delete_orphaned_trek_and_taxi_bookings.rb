065d400f3d65ceeabdf17cf69139cb4ed736e149
Preetam Reddy <preetam.reddy@gmail.com>
2012-02-29 15:18:50 +0530

    Skipping callbacks when deleting the orphaned records

--- db/migrate/20120229092250_delete_orphaned_trek_and_taxi_bookings.rb
+++ db/migrate/20120229092250_delete_orphaned_trek_and_taxi_bookings.rb
@@ -3,13 +3,13 @@ class DeleteOrphanedTrekAndTaxiBookings < ActiveRecord::Migration
 		TrekBooking.all.each do |tb|
 			trip = Trip.find_all_by_id(tb.trip_id)
 			if trip.count == 0
-				tb.destroy
+				tb.delete
 			end
 		end
 		TaxiBooking.all.each do |tb|
 			trip = Trip.find_all_by_id(tb.trip_id)
-			if trip.coint == 0
-				tb.destroy
+			if trip.count == 0
+				tb.delete
 			end
 		end
   end
