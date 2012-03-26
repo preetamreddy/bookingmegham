a1694ec77335611671fc771b41a5c558de60f29a
Preetam Reddy <preetam.reddy@gmail.com>
2012-03-26 18:59:55 +0530

    Redid the numtocurrency helper to it earlier state

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,20 +15,10 @@ module ApplicationHelper
 
 	def number_to_currency(number, html=true)
 		txt = html ? content_tag(:span, 'Rs. ', :class => :WebRupee) : 'Rs. '
-		if number >= 0
-			txt + "#{number.to_s.gsub(/(\d+?)(?=(\d\d)+(\d)(?!\d))(\.\d+)?/, "\\1,")}" 
-		else
-			number = 0 - number
-			txt + "(#{number.to_s.gsub(/(\d+?)(?=(\d\d)+(\d)(?!\d))(\.\d+)?/, "\\1,")})"
-		end
+		txt + "#{number.to_s.gsub(/(\d+?)(?=(\d\d)+(\d)(?!\d))(\.\d+)?/, "\\1,")}"
 	end
 
 	def number_to_currency_wo_symbol(number)
-		if number >= 0
-			"#{number.to_s.gsub(/(\d+?)(?=(\d\d)+(\d)(?!\d))(\.\d+)?/, "\\1,")}" 
-		else
-			number = 0 - number
-			"(#{number.to_s.gsub(/(\d+?)(?=(\d\d)+(\d)(?!\d))(\.\d+)?/, "\\1,")})"
-		end
+		"#{number.to_s.gsub(/(\d+?)(?=(\d\d)+(\d)(?!\d))(\.\d+)?/, "\\1,")}"
 	end
 end
