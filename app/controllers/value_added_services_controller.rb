cdf6be6eca460ef6afa139015d31452e24c0e4ae
Preetam Reddy <preetam.reddy@gmail.com>
2012-06-25 14:58:56 +0530

    Implemented auth for value added services

--- app/controllers/value_added_services_controller.rb
+++ app/controllers/value_added_services_controller.rb
@@ -1,8 +1,9 @@
 class ValueAddedServicesController < ApplicationController
+	load_and_authorize_resource
   # GET /value_added_services
   # GET /value_added_services.json
   def index
-    @value_added_services = ValueAddedService.order("property_id, name").all
+    @value_added_services = @value_added_services.order("property_id, name")
 
     respond_to do |format|
       format.html # index.html.erb
@@ -13,24 +14,15 @@ class ValueAddedServicesController < ApplicationController
   # GET /value_added_services/1
   # GET /value_added_services/1.json
   def show
-		begin
-    	@value_added_service = ValueAddedService.find(params[:id])
-		rescue ActiveRecord::RecordNotFound
-			logger.error "Attempt to access invalid value added service #{params[:id]}"
-			redirect_to value_added_services_url, alert: 'Invalid value added service'
-		else
-    	respond_to do |format|
-      	format.html # show.html.erb
-      	format.json { render json: @value_added_service }
-			end
+    respond_to do |format|
+     	format.html # show.html.erb
+     	format.json { render json: @value_added_service }
     end
   end
 
   # GET /value_added_services/new
   # GET /value_added_services/new.json
   def new
-    @value_added_service = ValueAddedService.new
-
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @value_added_service }
@@ -39,19 +31,11 @@ class ValueAddedServicesController < ApplicationController
 
   # GET /value_added_services/1/edit
   def edit
-		begin
-    	@value_added_service = ValueAddedService.find(params[:id])
-		rescue ActiveRecord::RecordNotFound
-			logger.error "Attempt to access invalid value added service #{params[:id]}"
-			redirect_to value_added_services_url, alert: 'Invalid value added service'
-		end
   end
 
   # POST /value_added_services
   # POST /value_added_services.json
   def create
-    @value_added_service = ValueAddedService.new(params[:value_added_service])
-
     respond_to do |format|
       if @value_added_service.save
         format.html { redirect_to @value_added_service, notice: 'Value added service was successfully created.' }
@@ -66,8 +50,6 @@ class ValueAddedServicesController < ApplicationController
   # PUT /value_added_services/1
   # PUT /value_added_services/1.json
   def update
-    @value_added_service = ValueAddedService.find(params[:id])
-
     respond_to do |format|
       if @value_added_service.update_attributes(params[:value_added_service])
         format.html { redirect_to @value_added_service, notice: 'Value added service was successfully updated.' }
@@ -82,7 +64,6 @@ class ValueAddedServicesController < ApplicationController
   # DELETE /value_added_services/1
   # DELETE /value_added_services/1.json
   def destroy
-    @value_added_service = ValueAddedService.find(params[:id])
     @value_added_service.destroy
 
     respond_to do |format|
