63ab5a2b7029f59e7aac906bb00b9466bfe7b0b5
Preetam Reddy <preetam.reddy@gmail.com>
2012-07-18 21:44:11 +0530

    Moved pay sts update code in billing observer to a separate method

--- app/models/billing_observer.rb
+++ app/models/billing_observer.rb
@@ -4,36 +4,29 @@ class BillingObserver < ActiveRecord::Observer
 	def after_save(model)
 		trip = model.trip
 		if trip
-			if trip.paid == 0
-				trip.update_attributes(:food_preferences => Trip::NOT_PAID,
-					:medical_constraints => (trip.created_at.to_date + 2).to_s )
-			else
-				if trip.balance_payment > 0
-					trip.update_attributes(:food_preferences => Trip::PARTIALLY_PAID,
-						:medical_constraints => (trip.start_date - 21).to_s )
-				else
-					trip.update_attributes(:food_preferences => Trip::FULLY_PAID,
-						:medical_constraints => nil)
-				end
-			end	
+			update_payment_status_and_pay_by_date(trip)
 		end
 	end
 
 	def after_destroy(model)
 		trip = model.trip
 		if trip
-			if trip.paid == 0
-				trip.update_attributes(:food_preferences => Trip::NOT_PAID,
-					:medical_constraints => (trip.created_at.to_date + 2).to_s )
-			else 
-				if trip.balance_payment > 0
-					trip.update_attributes(:food_preferences => Trip::PARTIALLY_PAID,
-					:medical_constraints => (trip.start_date - 21).to_s )
-				else
-					trip.update_attributes(:food_preferences => Trip::FULLY_PAID,
-					:medical_constraints => nil)
-				end
-			end	
+			update_payment_status_and_pay_by_date(trip)
 		end
+	end
+
+	def update_payment_status_and_pay_by_date(trip)
+		if trip.paid == 0
+			trip.update_attributes(:food_preferences => Trip::NOT_PAID,
+				:medical_constraints => (trip.created_at.to_date + 2).to_s )
+		else 
+			if trip.balance_payment > 0
+				trip.update_attributes(:food_preferences => Trip::PARTIALLY_PAID,
+				:medical_constraints => (trip.start_date - 21).to_s )
+			else
+				trip.update_attributes(:food_preferences => Trip::FULLY_PAID,
+				:medical_constraints => nil)
+			end
+		end	
 	end
 end
