b771592d5dc01346f3458d82d4eb4292b21db90b
Preetam Reddy <preetam.reddy@gmail.com>
2012-07-21 10:45:23 +0530

    Room observer is conditional now, only for bookings

--- app/models/room_observer.rb
+++ app/models/room_observer.rb
@@ -1,23 +1,29 @@
 class RoomObserver < ActiveRecord::Observer
 	def after_create(room)
-		delta_total_price = room.total_price
-		delta_service_tax = room.service_tax
-		update_price_for_rooms(room.booking, delta_total_price)
-		update_service_tax(room.booking, delta_service_tax)
+		if room.booking_id
+			delta_total_price = room.total_price
+			delta_service_tax = room.service_tax
+			update_price_for_rooms(room.booking, delta_total_price)
+			update_service_tax(room.booking, delta_service_tax)
+		end
 	end
 
 	def after_update(room)
-		delta_total_price = room.total_price - room.total_price_was
-		delta_service_tax = room.service_tax - room.service_tax_was
-		update_price_for_rooms(room.booking, delta_total_price)
-		update_service_tax(room.booking, delta_service_tax)
+		if room.booking_id
+			delta_total_price = room.total_price - room.total_price_was
+			delta_service_tax = room.service_tax - room.service_tax_was
+			update_price_for_rooms(room.booking, delta_total_price)
+			update_service_tax(room.booking, delta_service_tax)
+		end
 	end
 
 	def after_destroy(room)
-		delta_total_price = 0 - room.total_price
-		delta_service_tax = 0 - room.service_tax
-		update_price_for_rooms(room.booking, delta_total_price)
-		update_service_tax(room.booking, delta_service_tax)
+		if room.booking_id
+			delta_total_price = 0 - room.total_price
+			delta_service_tax = 0 - room.service_tax
+			update_price_for_rooms(room.booking, delta_total_price)
+			update_service_tax(room.booking, delta_service_tax)
+		end
 	end
 
 	def update_price_for_rooms(booking, delta)
