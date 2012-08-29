b40eea37a710f2baaf6e06fa455b45418f0b6fba
Preetam Reddy <preetam.reddy@gmail.com>
2012-08-29 15:57:51 +0530

    New validation: taxi booking is within trip dates; Removed: paysts check before destroy

--- app/models/taxi_booking.rb
+++ app/models/taxi_booking.rb
@@ -6,27 +6,36 @@ class TaxiBooking < ActiveRecord::Base
 		accepts_nested_attributes_for :taxi_details, :reject_if => :all_blank,
 		:allow_destroy => true
 
+  before_validation :update_end_date
+
 	validates :trip_id, :taxi_id, :start_date,
 						:number_of_vehicles, :number_of_days,
 						presence: true
 
+  validate :ensure_taxi_booking_is_within_trip_dates
+
 	before_save :strip_whitespaces, :titleize,
-							:update_end_date,
 							:update_unit_price, :update_total_price
 
-	before_destroy :ensure_payments_are_not_made
-
 	private
+		def update_end_date
+			self.end_date = start_date + number_of_days - 1
+		end
+
+		def ensure_taxi_booking_is_within_trip_dates
+			if (start_date < trip.start_date or start_date > trip.end_date) or
+        (end_date < trip.start_date or end_date > trip.end_date)
+				errors.add(:base, "Could not create taxi booking as it is not within the trip dates")
+				return false
+			end
+		end
+
 		def strip_whitespaces
 			self.pickup_address = pickup_address.to_s.strip
 		end
 
 		def titleize
 			self.drop_off_city = drop_off_city.titleize if drop_off_city
-		end
-
-		def update_end_date
-			self.end_date = start_date + number_of_days - 1
 		end
 
 		def update_unit_price
@@ -37,14 +46,5 @@ class TaxiBooking < ActiveRecord::Base
 
 		def update_total_price
 			self.total_price = unit_price * number_of_vehicles * number_of_days
-		end
-
-		def ensure_payments_are_not_made
-			if trip.payment_status == Trip::NOT_PAID
-				return true
-			else
-				errors.add(:base, "Could not delete taxi booking as payments have been made for the trip")
-				return false
-			end
 		end
 end
