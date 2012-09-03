876d2ea9a10b33b4eb5b0bfa063a4700bdf1068c
Preetam Reddy <preetam.reddy@gmail.com>
2012-09-03 20:54:44 +0530

    Account settings: Showing Name as Display Name, etc.

--- app/models/account_setting.rb
+++ app/models/account_setting.rb
@@ -7,22 +7,17 @@ class AccountSetting < ActiveRecord::Base
 	validates :phone_number_1,
     :format => { :with => /^[\+]?[\d\s]*$/, :message => "is not valid" }
 
-  after_update :update_name_abbreviation
-
   before_destroy :return_false
 
   private
     def set_defaults_if_nil
       self.tds_percent ||= 0
+      self.name_abbreviation ||= registered_name.split.collect { |w| w[0] }.join
     end
 
 		def strip_whitespaces
 			self.postal_address = postal_address.to_s.strip
 		end
-
-    def update_name_abbreviation
-      self.name_abbreviation = registered_name.split.collect { |w| w[0] }.join
-    end
 
     def return_false
       errors.add(:base, "Cannot destroy account settings")
